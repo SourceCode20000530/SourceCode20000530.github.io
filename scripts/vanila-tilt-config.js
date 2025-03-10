3bb0ffa63a212fe53a8768203bc3d7f1288d1913
Alexandr Kim <SourceCode20000530@gmail.com>
2025-03-10 17:13:23 +0000

    Update

--- scripts/vanila-tilt-config.js
+++ scripts/vanila-tilt-config.js
@@ -1,6 +1,14 @@
-VanillaTilt.init(document.querySelectorAll(".skillsBox"), {
-    max: 25,
-    speed: 400,
-    glare: true,
-    "max-glare": 1
-});
+headerElement = document.querySelector('header');
+
+if (!headerElement.classList.contains('sticky')) {
+    VanillaTilt.init(document.querySelectorAll(".skillsBox"), {
+        max: 25,
+        speed: 400,
+        glare: true,
+        "max-glare": 1
+    });
+}
+else {
+    VanillaTilt.init(document.querySelectorAll(""), {
+    });
+}
