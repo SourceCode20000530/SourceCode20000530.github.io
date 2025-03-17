e5bfcfa1cc21119b7c86d004c4e7ea291830e4f5
Alexandr Kim <SourceCode20000530@gmail.com>
2025-03-17 16:31:44 +0000

    Update

--- scripts/vanila-tilt-config.js
+++ scripts/vanila-tilt-config.js
@@ -1,3 +1,13 @@
+window.addEventListener('resize', () => {
+    if (window.innerWidth <= 1030) {
+        console.log("Ширина экрана <= 1030px")
+    }
+    else{
+        console.log("Ширина экрана > 1030px")
+    }
+})
+
+
 VanillaTilt.init(document.querySelectorAll(".skillsBox"), {
     max: 25,
     speed: 400,
