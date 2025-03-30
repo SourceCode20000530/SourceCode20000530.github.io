1f9921dbe628a1d50a1aa8fe4f11421fd0428b38
Alexandr Kim <SourceCode20000530@gmail.com>
2025-03-31 04:49:58 +0900

    Update

--- scripts/vanila-tilt-config.js
+++ scripts/vanila-tilt-config.js
@@ -1,16 +1,24 @@
+let vanillaTilt;
+
+const vanillaTiltWork = () => {
+    document.querySelectorAll('.skillsBox').forEach(element => {
+        if (window.innerWidth >= 1030)
+            vanillaTilt = new VanillaTilt(element, {
+                max: 25,
+                speed: 400,
+                glare: true,
+                "max-glare": 1
+            });             
+        else {
+            vanillaTilt = null;
+        }      
+    });
+}
+
+window.addEventListener('load', () => {
+    vanillaTiltWork()
+});
+
 window.addEventListener('resize', () => {
-    if (window.innerWidth <= 1030) {
-        console.log("Ширина экрана <= 1030px")
-    }
-    else{
-        console.log("Ширина экрана > 1030px")
-    }
-})
-
-
-VanillaTilt.init(document.querySelectorAll(".skillsBox"), {
-    max: 25,
-    speed: 400,
-    glare: true,
-    "max-glare": 1
+    vanillaTiltWork();
 });
